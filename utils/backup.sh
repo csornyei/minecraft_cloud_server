0b348a65fbbe9c39d5a3aa2fe30b9a882f240ea6
Mate Mark Csornyei <mate@csornyei.com>
2022-08-18 22:34:47 +0200

    zipping update

--- utils/backup.sh
+++ utils/backup.sh
@@ -3,6 +3,6 @@ bucket="csornyeicom-minecraft-server"
 
 file=/home/ubuntu/minecraft_server.zip
 
-/usr/bin/zip -r ${file} /home/ubuntu/minecraft_server
+/usr/bin/zip -r ${file} minecraft_server
 
 /usr/local/bin/aws s3 mv "${file}" "s3://${bucket}"
